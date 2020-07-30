f342cd54374b34020cb63c571ff9e79034724e88
sangryan <sangryan1@gmail.com>
2020-07-30 10:23:14 +0300

    first commit

---
+++ application/views/templates/footer.php
@@ -0,0 +1,15 @@
+
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs"><h5 style="font-family: tahoma;">Designed and Managed by <strong>Sang Ryan</strong>
+    </h5></div>
+    <strong>Copyright &copy;<?php echo date('Y') ?>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
